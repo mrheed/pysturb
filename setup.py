f21532eab4dfd8d6d4a6a2d2d6bc8543b70ace9c
syahidnurrohim <syahidnurrohim@gmail.com>
2020-07-28 21:38:25 +0700

    secod

--- setup.py
+++ setup.py
@@ -1,24 +1,52 @@
 import psutil
 import json
 import inquirer
+import socket
 
 # Print prettified json
 def jprint(args):
     print(json.dumps(args, indent=4, sort_keys=True))
 
-class MainController:
-    
+# Print pretiffied instance attributes
+def pprint(args):
+    jprint(vars(args))
+
+class Pysturb:
     def __init__(self):
-        iface = self.get_iface()
-        self.ip_addr = self.get_ip_addr()
+        self.iface_list = self.get_iface_list()
+        self.iface_selected = 'lo'
+        self.ip_addr = None
+        self.ipv6_addr = None
+        self.mac_addr = None
+
+    # Display interface selection menu
+    def prompt_select_iface(self):
+        questions = [
+            inquirer.List(
+                "iface",
+                message="Select network interface",
+                choices=self.iface_list
+            )
+        ]
+        ans = inquirer.prompt(questions)['iface']
+        self.iface_selected = ans
+        self.ip_addr = self.get_addr(ans)
+        self.ipv6_addr = self.get_addr(ans, t_addr='IPv6') 
+        self.mac_addr = self.get_addr(ans, t_addr='MAC')
 
     # Get available interfaces
-    def get_iface(self):
+    def get_iface_list(self):
         return [k for k in psutil.net_if_addrs()]
 
     # Get ip addres from selected interfaces
-    def get_ip_addr(self, iface='lo'):
-        return psutil.net_if_addrs()
+    def get_addr(self, iface='lo', t_addr='IPv4'):
+        af_map = {socket.AF_INET: 'IPv4', socket.AF_INET6: 'IPv6', psutil.AF_LINK: 'MAC'}
+        addrs = dict()
+        for addr in psutil.net_if_addrs()[iface]:
+            addrs[af_map.get(addr.family)] = addr.address
+        return addrs.get(t_addr)
 
-worker = MainController()
+worker = Pysturb()
+worker.prompt_select_iface()
+pprint(worker)
 
